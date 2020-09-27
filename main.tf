1746d6f985c5b6c4042a45d0c8143b7a71b216a1
Anand Banik <anandbanik@gmail.com>
2020-09-27 16:58:56 -0500

    upgraded

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "azurerm" {
 
-  version = "=2.4.0"
-
-   subscription_id = var.subscription_id
-   client_id       = var.client_id
-   client_secret   = var.client_secret
-   tenant_id       = var.tenant_id
-
+  version = "=2.5.0"
+  
+  subscription_id            = var.subscription_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
+  skip_provider_registration = true
   features {}
 }
 
